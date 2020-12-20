80d3fa97a47c6e85c34d02158f627ce2e409e1ec
Dinav <45940386+Dinav69@users.noreply.github.com>
2020-12-20 09:59:04 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const config = require("./config.json");
+
+
+client.login(config.BOT_TOKEN).catch(console.error);
+client.on('ready', () => {
+  console.log('Now Online, ready to filter!');
+});
+
+
+client.on('message', message => {
+  if(config.FILTER_LIST.some(word => message.content.toLowerCase().includes(word))){
+    message.delete()
+  }})
+
+ 
